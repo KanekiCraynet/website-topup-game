85e0b994a5461771b283b2575b006f425224a8a3
Phalosa Nugroho <osanugroho03@gmail.com>
2022-12-13 12:59:45 +0700

    Update metode.php

--- metode.php
+++ metode.php
@@ -1,12 +1,142 @@
+<?php
+ob_start();
+session_start();
+include('config/koneksi.php');
+$sid = session_id();
+$sql_0 = mysqli_query($conn,"SELECT * FROM `tb_seo` WHERE id = 1") or die(mysqli_error());
+$s0 = mysqli_fetch_array($sql_0);
+$urlweb = $s0['urlweb'];
+$urlwebs = $s0['urlweb'];
+$pengguna = $s0['user'];
+$sql_1a = mysqli_query($conn,"SELECT * FROM `tb_social` WHERE user = '$pengguna'") or die(mysqli_error());
+$s1a = mysqli_fetch_array($sql_1a);
+$sql_1b = mysqli_query($conn,"SELECT * FROM `tb_user` WHERE user = '$pengguna'") or die(mysqli_error());
+$s1b = mysqli_fetch_array($sql_1b);
+$ip = $_SERVER['REMOTE_ADDR'];
+$date = date('Y-m-d');
+$stat = mysqli_query($conn,"INSERT INTO `tb_stat` (`ip`, `date`, `hits`, `page`, `user`) VALUES ('$ip', '$date', 1, 'Daftar Harga', '$pengguna')") or die (mysqli_error());
+$sql_5 = mysqli_query($conn,"SELECT * FROM `tb_tripayapi` WHERE id = 1") or die(mysqli_error());
+$s5 = mysqli_fetch_array($sql_5);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Judul Halaman</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Daftar Harga - <?php echo $s0['instansi']; ?></title>
+  <meta name="description" content="<?php echo $s0['deskripsi']; ?>">
+  <meta name="keywords" content="<?php echo $s0['keyword']; ?>">
+  <meta property="og:title" content="Daftar Harga - <?php echo $s0['instansi']; ?>"/>
+  <meta property="og:description" content="<?php echo $s0['deskripsi']; ?>" />
+  <meta property="og:url" content="<?php echo $urlweb; ?>" />
+  <meta property="og:image" content="<?php echo $urlweb; ?>/upload/<?php echo $s0['image']; ?>" />
+  <meta name="resource-type" content="document" />
+  <meta http-equiv="content-type" content="text/html; charset=US-ASCII" />
+  <meta http-equiv="content-language" content="en-us" />
+  <meta name="author" content="Maks KuroZ Pedia" />
+  <meta name="contact" content="kurozpedia.my.id" />
+  <meta name="copyright" content="Copyright (c) kurozpedia.my.id. All Rights Reserved." />
+  <meta name="robots" content="index, nofollow">
+  
+  <!-- Icon-->
+  <link rel="shortcut icon" type="image/x-icon" href="<?php echo $urlweb; ?>/assets/images/tl-icon.png">
+
+  <link rel="stylesheet" href="<?php echo $urlweb; ?>/assets/plugins/summernote/dist/summernote-bs4.css"/>
+  <!-- simplebar CSS-->
+  <link href="<?php echo $urlweb; ?>/assets/plugins/simplebar/css/simplebar.css" rel="stylesheet"/>
+  <!-- Bootstrap core CSS-->
+  <link href="<?php echo $urlweb; ?>/assets/css/bootstrap.min.css" rel="stylesheet"/>
+  <!--Data Tables -->
+  <link href="<?php echo $urlweb; ?>/assets/plugins/bootstrap-datatable/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css">
+  <link href="<?php echo $urlweb; ?>/assets/plugins/bootstrap-datatable/css/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css">
+  <!-- animate CSS-->
+  <link href="<?php echo $urlweb; ?>/assets/css/animate.css" rel="stylesheet" type="text/css"/>
+  <!-- Icons CSS-->
+  <link href="<?php echo $urlweb; ?>/assets/css/icons.css" rel="stylesheet" type="text/css"/>
+  <!-- Horizontal menu CSS-->
+  <link href="<?php echo $urlweb; ?>/assets/css/horizontal-menu.css" rel="stylesheet"/>
+  <!-- Custom Style-->
+  <link href="<?php echo $urlweb; ?>/assets/css/app-style.css" rel="stylesheet"/>
+  <link href="<?php echo $urlweb; ?>/assets/css/style-main.css" rel="stylesheet"/>
+  <!-- Custom Style--> 
 </head>
+
 <body>
-<H1></H1>
-<p></p>	
+
+  <!-- Start wrapper-->
+  <div id="wrapper">
+
+    <!--Start topbar header-->
+    <?php include('top_menu.php'); ?>
+    <!--End topbar header-->
+
+    <div class="clearfix pt-5"></div>
+    <div class="pt-5 pb-5">
+      <div class="container">
+       <div class="row">
+          <div class="col-lg-3">
+            <div class="pt-3 pb-4">
+              <h5>Metode Pembayaran</h5>
+              <span class="strip-primary"></span>
+            </div>
+          </div>
+          <div class="col-lg-9">
+            <div class="pb-3">
+              <div class="section">
+                <div class="card-body">
+                  <div class="row pt-3 pl-2 pr-2 mb-2" id="result">
+                    <?php
+                      $apiKey = $s5['api_key'];
+                      $curls = curl_init();
+                               
+                      curl_setopt_array($curls, array(
+                        CURLOPT_FRESH_CONNECT     => true,
+                        CURLOPT_URL               => "https://tripay.co.id/api/merchant/payment-channel",
+                        CURLOPT_RETURNTRANSFER    => true,
+                        CURLOPT_IPRESOLVE         => CURL_IPRESOLVE_V4,
+                        CURLOPT_HEADER            => false,
+                        CURLOPT_HTTPHEADER        => array(
+                          "Authorization: Bearer ".$apiKey
+                        ),
+                        CURLOPT_FAILONERROR       => false
+                      ));
+                                
+                      $responses = curl_exec($curls);
+                      $errs = curl_error($curls);
+                      curl_close($curls);
+                      //echo !empty($err) ? $err : $responses;
+                      $hasils = json_decode($responses, true);
+                      for ($i=0; $i < count($hasils['data']); $i++) {
+                        if($hasils['data'][$i]['active'] == 'true'){
+                  ?>
+                  <div class="col-6 mb-4">
+                    <div class="metode-bayar">
+                      <img src="<?php echo $hasils['data'][$i]['icon_url']; ?>" class="img-fluid text-center" style="margin-bottom: -10px; display: block; width: auto; height: 35px;">
+                      <hr>
+                      <p style="font-weight: normal; color: #000;"><?php echo $hasils['data'][$i]['name']; ?></p>
+                    </div>
+                  </div>
+                  <?php
+                      }
+                    }
+                  ?>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  	
+    
+	  <div class="d-block d-sm-none" style="height: 100px;"></div>
+    <!--Start Back To Top Button-->
+    <a href="javaScript:void();" class="back-to-top"><i class="fa fa-angle-double-up"></i> </a>
+    <!--End Back To Top Button-->
+	
+	  <!--Start footer-->
+    <?php include('footer.php'); ?>
 </body>
 </html>
