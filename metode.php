465e789d9ed6e8931d202e6ba3fa76ea2025bc86
Phalosa Nugroho <osanugroho03@gmail.com>
2022-12-13 12:55:02 +0700

    Create metode.php

---
+++ metode.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Judul Halaman</title>
+</head>
+<body>
+<H1></H1>
+<p></p>	
+</body>
+</html>
